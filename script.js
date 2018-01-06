bd904afbb18301687b9a00a944e88a60358eca12
Leon Morgan <lendoe@aol.com>
2018-01-06 18:31:15 -0500

    Create on click events

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     img1: "./assets/img1.png",
     img2: "./assets/img2.png",
     img3: "./assets/img3.png"
-
   }
+
   function createRow(howManyEl){
     for(let i = 1; i <= howManyEl; i++){
-      $(".squares").append(`<div><img src="${imageUrls["img"+i]}" alt="img${i}" height="100" width="100"></div>`);
+      $(".squares").append(`<div class="${"img" + i}"></div>`);
     }
     $(".squares").append("<br>"); //remove this line when creating css
   }
@@ -17,7 +17,7 @@ $(document).ready(function() {
   function giveClasses() {
     let i = 1;
     $(".squares > div").each(function(){
-      $(this).addClass("box" + i);
+      $(this).addClass("img" + i);
       i += 1;
     })
   }
@@ -26,15 +26,20 @@ $(document).ready(function() {
     for (let i = 0; i < howManyRows; i++) {
       createRow(howManyEl);
     }
-    giveClasses();
   }
 
   createSquare(2, 2);
 
+  $(".squares > div").click(function() {
+    $(this).append(`<img src="${imageUrls[$(this).attr("class")]}" height="100" width="100">`);
+  })
 
 
-
-
+//create function to shuffle image divs
+  //maybe copy divs into an array, delete the divs, shuffle the array, place back divs
+//create function to delete image divs if two are clicked and match, otherwise flip them back facedown
+  //flipping card is just a matter of adding/removing the image tags within each div
+    //maybe create a function to handle "flipping" the cards
 
 
 
