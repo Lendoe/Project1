45f8577eef1b9906fb4e183c9d1e32f481e5c170
Leon Morgan <lendoe@aol.com>
2018-01-06 16:14:49 -0500

    created functions to build board

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
   console.log("hi jquery");
+
+  function createRow(howManyEl){
+    for(let i = 1; i <= howManyEl; i++){
+      $(".squares").append(`<div>${i}</div>`);
+    }
+    $(".squares").append("<br>"); //remove this line when creating css
+  }
+
+  function giveClasses() {
+    let i = 1;
+    $(".squares > div").each(function(){
+      $(this).addClass("box" + i);
+      i += 1;
+    })
+  }
+
+  function createSquare(howManyRows, howManyEl) {
+    for (let i = 0; i < howManyRows; i++) {
+      createRow(howManyEl);
+    }
+    giveClasses();
+  }
+
+  createSquare(3, 3);
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
