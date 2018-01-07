4d7d9ccd31961ee534acfd11452126189aad73d8
Leon Morgan <lendoe@aol.com>
2018-01-06 21:22:54 -0500

    added/fixed logic

--- script.js
+++ script.js
@@ -7,33 +7,41 @@ $(document).ready(function() {
     img3: "./assets/img3.png"
   }
 
-  function createRow(howManyEl){
-    for(let i = 1; i <= howManyEl; i++){
+  function createRow(howManyCol){
+    for(let i = 1; i <= howManyCol; i++){
       $(".squares").append(`<div class="${"img" + i}"></div>`);
     }
     $(".squares").append("<br>");
   }
 
-  function createSquare(howManyRows, howManyEl) {
+  function createSquare(howManyRows, howManyCol) {
     for (let i = 0; i < howManyRows; i++) {
-      createRow(howManyEl);
+      createRow(howManyCol);
     }
   }
 
+  //create the game board
   createSquare(2, 2);
 
+  console.log($(".squares > div").find("img"));
+  // click function which adds an image to each div
   $(".squares > div").click(function() {
-    $(this).append(`<img src="${imageUrls[$(this).attr("class")]}" height="100" width="100">`);
+    // create condition to limit appending img tags
+    if ($(this).children("img").length == 0) {
+      $(this).append(`<img src="${imageUrls[$(this).attr("class")]}" height="100" width="100">`);  
+    }
   })
 
 
-//create function to shuffle image divs
-  //maybe copy divs into an array, delete the divs, shuffle the array, place back divs
+  // function hideDivs(){
+  //   if()
+  // }
+
 //create function to delete image divs if two are clicked and match, otherwise flip them back facedown
   //flipping card is just a matter of adding/removing the image tags within each div
     //maybe create a function to handle "flipping" the cards
-
-
+//create function to shuffle image divs
+  //maybe copy divs into an array, delete the divs, shuffle the array, place back divs
 
 
 
